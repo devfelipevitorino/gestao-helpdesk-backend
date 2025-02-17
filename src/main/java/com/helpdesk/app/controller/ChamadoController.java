6628b98859bc3dc1eb69806734379a3ce58f0884
devfelipevitorino <devfelipevitorino@gmail.com>
2025-02-17 08:52:55 -0300

    metodo findbyid para chamados

---
+++ src/main/java/com/helpdesk/app/controller/ChamadoController.java
@@ -0,0 +1,26 @@
+package com.helpdesk.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.helpdesk.app.DTO.ChamadoDTO;
+import com.helpdesk.app.domain.Chamado;
+import com.helpdesk.app.services.ChamadoService;
+
+@RestController
+@RequestMapping("/chamados")
+public class ChamadoController {
+	
+	@Autowired
+	private ChamadoService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
+		Chamado obj = service.findById(id);
+		return ResponseEntity.ok().body(new ChamadoDTO(obj));		
+	}
+}
---
+++ src/main/java/com/helpdesk/app/services/ChamadoService.java
@@ -0,0 +1,23 @@
+package com.helpdesk.app.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.helpdesk.app.domain.Chamado;
+import com.helpdesk.app.exception.ObjectNotFoundException;
+import com.helpdesk.app.repositories.ChamadoRepository;
+
+@Service
+public class ChamadoService {
+	
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+
+}
