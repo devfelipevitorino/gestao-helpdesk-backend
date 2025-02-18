59e3b01368952998752e57b16ccc7dd6da2bfeed
devfelipevitorino <devfelipevitorino@gmail.com>
2025-02-17 22:38:04 -0300

    autorizacao jwt

--- src/main/java/com/helpdesk/app/config/SecurityConfig.java
+++ src/main/java/com/helpdesk/app/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,9 +19,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.helpdesk.app.security.JWTUtil;
 import com.helpdesk.app.security.JwtAuthenticationFilter;
+import com.helpdesk.app.security.JwtAuthorizationFilter;
 
 @SuppressWarnings("deprecation")
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	private static final String[] PUBLIC_MATHERS = {"/h2-console/**"};
@@ -42,6 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		
 		http.cors().and().csrf().disable();
 		http.addFilter(new JwtAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JwtAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.authorizeRequests()
 			.antMatchers(PUBLIC_MATHERS).permitAll()
 			.anyRequest().authenticated();
